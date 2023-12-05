67897ff160fd114d59c79807ed090423055a6711
Steven Kwan <stevent.kwan@gmail.com>
2023-12-05 07:09:03 -0800

    Created config folder and a new file for postgresql db configuration. Included cors library in app.js entry file.

--- routes/api/helloworld.js
+++ routes/api/helloworld.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/helloworld', (req, res) => {
   // Test process.env variables
-  console.log(`password is ${process.env.DB_PASSWORD}`);
+  console.log(`username is ${process.env.DB_USER}`);
 
   // Test JSON response
   res.status(200).json({ message: 'hello world' });
