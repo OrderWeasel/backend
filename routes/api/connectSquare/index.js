b7566a3f84dbfd2be9669262b8addda030cd1003
Steven Kwan <stevent.kwan@gmail.com>
2024-03-02 01:10:15 -0800

    Updated handler to use a nonce in `state` url query parameter of authorization url

--- routes/api/connectSquare/index.js
+++ routes/api/connectSquare/index.js
@@ -1,65 +1,54 @@
 const express = require('express');
+const crypto = require('node:crypto');
+const format = require('pg-format');
+const { createClient } = require('redis');
+const db = require('../../../config/db');
+const { generateCodeVerifier, generateCodeChallenge } = require('./helpers');
 
 const router = express.Router();
 
-// Goal: Obtain merchant "access token" and "refresh token"
-// from using Square API and save to merchant record.
-
-const CryptoJS = require("crypto-js");
-const crypto = require('node:crypto');
-const format = require('pg-format');
-const db = require('../../../config/db');
-
-function base64URLEncode(str) {
-  return str.toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '');
-}
-
-function generateCodeVerifier() {
-  return base64URLEncode(crypto.randomBytes(32));
-}
-
-function generateCodeChallenge(codeVerifier) {
-  const codeChallenge = base64URL(CryptoJS.SHA256(codeVerifier));
-  return codeChallenge;
-}
-
-function base64URL(string) {
-  return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
-}
-
 router.get('/geturl/:id', async (req, res) => {
   const { id } = req.params;
+
+  // Connect to Redis client
+  const redisClient = createClient();
+  redisClient.connect();
+
+  // Check if merchant has already provided authorization,
+  // if so, send appropriate response. Else:
+
+  // Generate nonce
+  const nonce = crypto.randomBytes(16).toString('base64');
+
+  console.log(`The nonce is ${nonce}`);
+  console.log(`The session id is ${String(req.session.id)}`);
+  console.log('-----------------------------------------');
+  console.log('-----------------------------------------');
+
+  // Save to redisClient key=nonce value=req.session.id
+  await redisClient.set(nonce, String(req.session.id));
+
+  // Disconnect from redisClient
+  await redisClient.disconnect();
+
+  // Generate code_verifier and code_challenge url params
   const columnName = 'code_verifier';
   const codeVerifier = generateCodeVerifier();
   const codeChallenge = generateCodeChallenge(codeVerifier);
-  const url = `https://connect.squareupsandbox.com/oauth2/authorize?scope=ITEMS_READ%20ORDERS_WRITE%20ORDERS_READ%20PAYMENTS_WRITE&client_id=${process.env.SQ_SANDBOX_APP_ID}&code_challenge=${codeChallenge}`;
+
+  // Include nonce in the url query params
+  const url = `https://connect.squareupsandbox.com/oauth2/authorize?state=${nonce}session=false&scope=ITEMS_READ&client_id=${process.env.SQ_SANDBOX_APP_ID}&code_challenge=${codeChallenge}`;
   const sql = format('UPDATE merchants SET %I = %L WHERE id = %s RETURNING *', columnName, codeVerifier, id);
-  let updatedMerchant;
+
+  // Save code_verifier to database
   try {
-    updatedMerchant = await db.query(sql);
+    await db.query(sql);
   } catch (error) {
     console.log(error);
   }
-  console.log(updatedMerchant.rows[0]);
+
+  // Respond with Square authroization URL
   res.status(200).json({ codeVerifier, codeChallenge, url });
-});
-
-// Process the authorization token
-router.get('/oauth-redirect', async (req, res) => {
-  
-});
-
-router.patch('/:id', async (req, res) => {
-  const merchantId = req.params.id;
-  res.status(200).json({ message: `PATCH method; connect Square where merchant_id = ${merchantId}` });
-
-  // Square API?
-  // Send URL
-
-  
 });
 
 module.exports = router;
